767ece26b8357ce189daec2e782ce5fd12f943b1
mrciao01 <botciao12@gmail.com>
2024-10-11 23:19:32 +0200

    Update risposta_comw.js

--- plugins/risposta_comw.js
+++ plugins/risposta_comw.js
@@ -211,7 +211,7 @@ const handler = async (m, { command, text }) => {
   m.reply(randomResponse.trim(), null, m.mentionedJid ? { mentions: m.mentionedJid } : {});
 };
 
-handler.customPrefix = /come/i;
+handler.customPrefix = /%;
 handler.command = new RegExp;
 
 export default handler;
